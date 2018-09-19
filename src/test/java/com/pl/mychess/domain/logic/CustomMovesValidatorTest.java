e21890ffd3e69379eab928a89dafcbe2b2e526fa
Maćki Ido <maciej.macko888@gmail.com>
2018-09-19 16:08:02 +0200

    add tests structure in CustomMovesValidatorTest class

--- src/test/java/com/pl/mychess/domain/logic/CustomMovesValidatorTest.java
+++ src/test/java/com/pl/mychess/domain/logic/CustomMovesValidatorTest.java
@@ -1,4 +1,120 @@
 package com.pl.mychess.domain.logic;
 
+import org.junit.Test;
+
 public class CustomMovesValidatorTest {
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheChosenFigureIsNotAKing(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheKingWasMoved(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheRookWasMoved(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheKingIsChecked(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures(){
+
+    }
+
+    @Test
+    public void shouldShortCastleValidatorReturnTrueWhenCastleMoveIsCorrect(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheChosenFigureIsNotAKing(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheKingWasMoved(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheRookWasMoved(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheKingIsChecked(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures(){
+
+    }
+
+    @Test
+    public void shouldLongCastleValidatorReturnTrueWhenCastleMoveIsCorrect(){
+
+    }
+
+    @Test
+    public void shouldEnPassantValidatorReturnFalseWhenTheChosenFigureIsNotAPawn(){
+
+    }
+
+    @Test
+    public void shouldEnPassantValidatorReturnFalseWhenTheLastMovedFigureIsNotAPawn(){
+
+    }
+
+    @Test
+    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotStandBesideTheBeatingPawn(){
+
+    }
+
+    @Test
+    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotMoveAboutTwoPlacesForward(){
+
+    }
+
+    @Test
+    public void shouldEnPassandValidatorReturnTrueWhenEnPassantMoveIsCorrect(){
+
+    }
 }
