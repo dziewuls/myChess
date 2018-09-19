c42a2d1e2f13c8ae19bfe7a7fc3dc47287f2cf7a
Maćki Ido <maciej.macko888@gmail.com>
2018-09-19 18:14:19 +0200

    fill the castling validate tests

--- src/test/java/com/pl/mychess/domain/logic/CustomMovesValidatorTest.java
+++ src/test/java/com/pl/mychess/domain/logic/CustomMovesValidatorTest.java
@@ -1,120 +1,307 @@
 package com.pl.mychess.domain.logic;
 
+import com.pl.mychess.domain.chessboard.ChessboardFactory;
+import com.pl.mychess.domain.model.chessboard.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomMovesValidatorTest {
     @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheChosenFigureIsNotAKing(){
+    public void shouldShortCastlingValidatorReturnFalseWhenTheChosenFigureIsNotAKing() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedPawn);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('g', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheKingWasMoved() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        testedKing.setMoved(true);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheRookWasMoved() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        testedRook.setMoved(true);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheKingIsChecked() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBishop = new Figure(TypeOfFigure.BISHOP, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('h', 4, testedBishop);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBlackRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('g', 4, testedBlackRook);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBlackRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('f', 4, testedBlackRook);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBishop = new Figure(TypeOfFigure.BISHOP, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+        chessboard.setFigureInPlace('f', 1, testedBishop);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldShortCastlingValidatorReturnTrueWhenCastlingMoveIsCorrect() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('h', 1, testedRook);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isTrue();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheChosenFigureIsNotAKing() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedPawn);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 2, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheKingWasMoved() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        testedKing.setMoved(true);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheRookWasMoved() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        testedRook.setMoved(true);
+        Figure testedPawn = new Figure(TypeOfFigure.PAWN, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('e', 2, testedPawn);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheKingIsChecked() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBishop = new Figure(TypeOfFigure.BISHOP, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('h', 4, testedBishop);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBlackRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('c', 4, testedBlackRook);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBlackRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.BLACK);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('d', 4, testedBlackRook);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        Figure testedBishop = new Figure(TypeOfFigure.BISHOP, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+        chessboard.setFigureInPlace('c', 1, testedBishop);
+
+        boolean longCastlingCorrect = CustomMovesValidator.isLongCastlingCorrect(chessboard, testedKing);
+
+        assertThat(longCastlingCorrect).isFalse();
+    }
+
+    @Test
+    public void shouldLongCastlingValidatorReturnTrueWhenCastlingMoveIsCorrect() {
+        Chessboard chessboard = ChessboardFactory.createEmptyChessboard();
+        Figure testedKing = new Figure(TypeOfFigure.KING, ColorOfFigure.WHITE);
+        Figure testedRook = new Figure(TypeOfFigure.ROOK, ColorOfFigure.WHITE);
+        chessboard.setFigureInPlace('e', 1, testedKing);
+        chessboard.setFigureInPlace('a', 1, testedRook);
+
+        boolean shortCastlingCorrect = CustomMovesValidator.isShortCastlingCorrect(chessboard, testedKing);
+
+        assertThat(shortCastlingCorrect).isTrue();
+    }
+
+    @Test
+    public void shouldEnPassantValidatorReturnFalseWhenTheChosenFigureIsNotAPawn() {
 
     }
 
     @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace(){
+    public void shouldEnPassantValidatorReturnFalseWhenTheLastMovedFigureIsNotAPawn() {
 
     }
 
     @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheKingWasMoved(){
+    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotStandBesideTheBeatingPawn() {
 
     }
 
     @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheRookWasMoved(){
+    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotMoveAboutTwoPlacesForward() {
 
     }
 
     @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheKingIsChecked(){
-
-    }
-
-    @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace(){
-
-    }
-
-    @Test
-    public void shouldShortCastleValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace(){
-
-    }
-
-    @Test
-    public void shouldShortCastleValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures(){
-
-    }
-
-    @Test
-    public void shouldShortCastleValidatorReturnTrueWhenCastleMoveIsCorrect(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheChosenFigureIsNotAKing(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheRookIsNotInTheCorrectPlace(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheKingWasMoved(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheRookWasMoved(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheKingIsChecked(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheKingStandsInTheCheckedPlace(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenTheKingPassesThroughTheCheckedPlace(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnFalseWhenBetweenTheKingAndTheRookAreSomeFigures(){
-
-    }
-
-    @Test
-    public void shouldLongCastleValidatorReturnTrueWhenCastleMoveIsCorrect(){
-
-    }
-
-    @Test
-    public void shouldEnPassantValidatorReturnFalseWhenTheChosenFigureIsNotAPawn(){
-
-    }
-
-    @Test
-    public void shouldEnPassantValidatorReturnFalseWhenTheLastMovedFigureIsNotAPawn(){
-
-    }
-
-    @Test
-    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotStandBesideTheBeatingPawn(){
-
-    }
-
-    @Test
-    public void shouldEnPassantValidatorReturnFalseWhenTheBeatenPawnNotMoveAboutTwoPlacesForward(){
-
-    }
-
-    @Test
-    public void shouldEnPassandValidatorReturnTrueWhenEnPassantMoveIsCorrect(){
+    public void shouldEnPassandValidatorReturnTrueWhenEnPassantMoveIsCorrect() {
 
     }
 }
